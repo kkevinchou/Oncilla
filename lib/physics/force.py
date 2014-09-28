d345682ec51dce3180ee9ec59b55f50eb1920f85
Kevin Chou <kchou@tinyco.com>
2014-09-28 12:43:06 -0700

    adding force.py

---
+++ lib/physics/force.py
@@ -0,0 +1,27 @@
+from lib.vec2d import Vec2d
+
+class Force(object):
+    def __init__(self, vector):
+        self._vector = vector.copy()
+
+    def update(self, delta):
+        return True
+
+    @property
+    def vector(self):
+        return self._vector.copy()
+
+    @vector.setter
+    def vector(self, val):
+        raise Exception('Reassigning vector to force is disallowed')
+
+class TimedForce(Force):
+    def __init__(self, vector, duration):
+        super(TimedForce, self).__init__(vector)
+        
+        self.elapsed_time = 0
+        self.duration = duration
+
+    def update(self, delta):
+        self.elapsed_time += delta
+        return self.elapsed_time <= self.duration
